4a1518d8acc4d1800c0423c0afd6059ddbc8187a
navi0407 <ivanjohngawayen@gmail.com>
2025-12-09 21:32:48 +0800

    Polished the code in ExpenseEntryForm

--- PersonalExpenseTracker_OOP/IncomeEntryForm.cs
+++ PersonalExpenseTracker_OOP/IncomeEntryForm.cs
@@ -151,39 +151,5 @@ namespace PersonalExpenseTracker_OOP
             cmbCategory.Text = row.Cells["Category"].Value.ToString();
             txtDescription.Text = row.Cells["Description"].Value.ToString();
         }
-
-
-        //private void btnSave_Click(object sender, EventArgs e)
-        //{
-
-        //    if (string.IsNullOrWhiteSpace(txtAmount.Text) ||
-        //        !decimal.TryParse(txtAmount.Text, out decimal amount) ||
-        //        amount <= 0)
-        //    {
-        //        MessageBox.Show("Please enter a valid amount.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-        //        return;
-        //    }
-
-        //    string description = txtDescription.Text.Trim();
-        //    string date = dtpDate.Value.ToString("yyyy-MM-dd");
-
-        //    using (var conn = new SQLiteConnection(cs))
-        //    {
-        //        conn.Open();
-        //        using (var cmd = new SQLiteCommand(
-        //            @"INSERT INTO Transactions (Type, Amount, Date, Description)
-        //                  VALUES ('Income', @amount, @date, @description);", conn))
-        //        {
-        //            cmd.Parameters.AddWithValue("@amount", amount);
-        //            cmd.Parameters.AddWithValue("@date", date);
-        //            cmd.Parameters.AddWithValue("@description", description);
-        //            cmd.ExecuteNonQuery();
-        //        }
-        //    }
-
-        //    MessageBox.Show("Income entry saved.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-        //    this.Close();
-        //}
-
     }
 }
